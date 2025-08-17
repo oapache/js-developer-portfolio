2b693dca79779fbd830c039e48bb6304b54fb113
Pablo Henrique <160090075+oapache@users.noreply.github.com>
2025-08-17 01:08:43 -0300

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,63 +1,7 @@
-{
- "name": "Pablo Henrique",
- "photo": "https://avatars.githubusercontent.com/u/160090075?v=4",
- "job": "Developer Full Stack",
- "location": "São Paulo - SP",
- "phone": "(11) 947920549",
- "email": "pablohenrique2k2@gmail.com",
- "skills": {
-   "hardSkills": [
-     {
-       "name": "Nodejs",
-       "logo": "https://raw.githubusercontent.com/digitalinnovationone/js-developer-portfolio/main/data/imgs/nodejs.png"
-     },
-     {
-       "name": "js",
-       "logo": "https://raw.githubusercontent.com/digitalinnovationone/js-developer-portfolio/main/data/imgs/js.png"
-     },
-     {
-       "name": "CSS",
-       "logo": "https://raw.githubusercontent.com/digitalinnovationone/js-developer-portfolio/main/data/imgs/css.png"
-     },
-     {
-       "name": "Mongodb",
-       "logo": "https://raw.githubusercontent.com/digitalinnovationone/js-developer-portfolio/main/data/imgs/mongodb.png"
-     },
-     {
-       "name": "Postgresql",
-       "logo": "https://raw.githubusercontent.com/digitalinnovationone/js-developer-portfolio/main/data/imgs/postgresql.png"
-     }
-   ],
-   "softSkills": [
-     "Liderança",
-     "Trabalho em equipe",
-     "Agile",
-     "Adaptabilidade e empatia"
-   ]
- },
- "languages": [
-   "Português BR",
-   "Inglês (B1)",
-   "Espanhol (Básico)"
- ],
- "portfolio": [
-  {
-    "name": "Curso Ministrado na DIO para a criação de uma Landing Page",
-    "url": "https://github.com/oapache/trilha-css-desafio-01",
-    "github": true
-  },
-   {
-     "name": "Curso Ministrado na DIO para a criação de uma Pokedex",
-     "url": "https://github.com/oapache/js-developer-pokedex",
-     "github": true
-   },
-   {
-     "name": "Curso Ministrado na DIO para a criação de uma página de portfolio",
-     "url": "https://github.com/oapache/js-developer-portfolio",
-     "github": true
-   }
-   
- ]
 
- 
+async function fetchProfileData() {
+    const url = 'https://raw.githubusercontent.com/digitalinnovationone/js-developer-portfolio/main/data/profile.json';
+    const response = await fetch(url)
+    const profileData = await response.json()
+    return profileData
 }
